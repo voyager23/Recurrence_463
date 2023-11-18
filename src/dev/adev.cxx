94639ab7db01f549a84a5107a716248a1ac7d5bb
Mike <Mike@Aorus_F38>
2023-11-18 18:18:19 +0100

    updates to adev

--- src/dev/adev.cxx
+++ src/dev/adev.cxx
@@ -19,14 +19,16 @@ void prt_vect(vector<uint64_t> &v){
 
 int main(int argc, char **argv)
 {
-	vector<uint64_t> f(50,0);
-	f[1] = 1;
-	f[3] = 3;
+	vector<uint64_t> f;
+	vector<uint64_t> g = {0,1,0,3};
+	f.insert(f.end(), g.begin(), g.end());
 	prt_vect(f);
 	for(uint64_t i = 1; i != 9; ++i){
 		f[(2*i)] = f[i];
-		f[(4*i + 1)] = 2*f[(2*i + 1)] - f[i];
-		f[(4*i + 3)] = 3*f[(2*i + 1)] - 2*f[i];
+		//~ f[(4*i + 1)] = 2*f[(2*i + 1)] - f[i];
+		//~ f[(4*i + 3)] = 3*f[(2*i + 1)] - 2*f[i];
+		g = {0, 2*f[(2*i + 1)] - f[i], 0, 3*f[(2*i + 1)] - 2*f[i]};
+		f.insert(f.end(), g.begin(), g.end());
 		prt_vect(f);
 		if(i == 4) break;
 	}
