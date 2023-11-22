b40a706993d18ad0bc6a64b9635f8747f76c945f
Mike <mike@ThinkpadX1.home>
2023-11-22 14:19:51 +0100

    Updates to github

--- src/dev/bdev.cxx
+++ src/dev/bdev.cxx
@@ -22,17 +22,18 @@ int main(int argc, char **argv)
 	vector<uint64_t> g = {0,1,0,3};
 	f.insert(f.end(), g.begin(), g.end());
 	prt_vect(f);
-	for(uint64_t i = 1; i != 10; ++i){
+	for(uint64_t i = 1; i != 19; ++i){
 		f[(2*i)] = f[i];
 		g = {0,  2*f[(2*i + 1)] - f[i],  0,  3*f[(2*i + 1)] - 2*f[i] };
 		f.insert(f.end(), g.begin(), g.end());
 		cout << "i:" << i << "  ";
-		prt_vect(f);
+		//prt_vect(f);
 		uint64_t S = 0;
 		uint64_t j = 0;
 		while(j <= i) S += f[j++];
 		cout << "S[" << i << "] = " << S << endl; 
 	}
+	prt_vect(f);
 	
 
 	
